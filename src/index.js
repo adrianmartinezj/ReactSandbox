1de7a250737579c72c2430489edac100fe2af03e
adrianjustinmartinez@gmail.com <adrianjustinmartinez@gmail.com>
2019-05-22 17:01:30 -0700

    Up to components

--- src/index.js
+++ src/index.js
@@ -5,27 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-  }
-  
-  const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
-  };
-  
-  const element = (
-    <h1>
-      Hello, {formatName(user)}!
-    </h1>
-  );
-  
-  function getGreeting(user) {
-    if (user) {
-      return <h1>Hello, {formatName(user)}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
-  }
+// function formatName(user)
 
 // const name = 'Josh Perez';
 // const element = <h1>Hello, {name}</h1>;
@@ -43,10 +23,119 @@ function formatName(user) {
 //   <h2>Good to see you here.</h2>
 // </div>
 // );
+// ReactDOM.render(getGreeting(user), document.getElementById('root'));
 
-ReactDOM.render(getGreeting(user), document.getElementById('root'));
+// Typical way to render what we want
+// const element = <h1>Hello, world</h1>;
+// ReactDOM.render(element, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000);
+
+
+// Functionally the same to React
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>;
+//   }
+// }
+
+// PROPS = properties, will be passed in as one object.
+// function Welcome(props) {
+//   // Can extract values from props like 'props.name'
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// const element = <Welcome name="Bobby bwaaaa" />;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="Sara" />
+//       <Welcome name="Cahal" />
+//       <Welcome name="Edite" />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+// DO NOT CHANGE PROPS WHEN PASSED IN
+
+// OK 
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// NOT OK 
+// function withdraw(account, amount) {
+//   account.total -= amount;
+// }
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root'));
+
+
 serviceWorker.unregister();
